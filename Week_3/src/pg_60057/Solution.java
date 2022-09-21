233244c21facacd9663261015bf87cbcca931b25
mon0mon <mon0mon@naver.com>
2022-09-21 23:05:21 +0900

    pg_600057 fail2

--- Week_3/src/pg_60057/Solution.java
+++ Week_3/src/pg_60057/Solution.java
@@ -2,46 +2,48 @@ package pg_60057;
 
 public class Solution {
     public static void main(String[] args) {
-//        System.out.println(solution("xababcdcdababcdcd"));
+//        System.out.println(solution("ababcdcdababcdcd"));
     }
 
-    //테스트 1 〉	통과 (3.38ms, 81.8MB)
-    //테스트 2 〉	실패 (7.50ms, 79.5MB)
-    //테스트 3 〉	통과 (4.87ms, 71.9MB)
-    //테스트 4 〉	실패 (런타임 에러)
-    //테스트 5 〉	실패 (0.04ms, 73.3MB)
-    //테스트 6 〉	실패 (런타임 에러)
-    //테스트 7 〉	실패 (런타임 에러)
-    //테스트 8 〉	실패 (런타임 에러)
-    //테스트 9 〉	실패 (런타임 에러)
-    //테스트 10 〉	통과 (32.08ms, 94.4MB)
-    //테스트 11 〉	통과 (3.65ms, 74.6MB)
-    //테스트 12 〉	통과 (4.67ms, 77.5MB)
-    //테스트 13 〉	통과 (5.98ms, 72.2MB)
-    //테스트 14 〉	통과 (11.31ms, 82.3MB)
-    //테스트 15 〉	통과 (4.53ms, 81.3MB)
-    //테스트 16 〉	통과 (2.56ms, 74.5MB)
-    //테스트 17 〉	실패 (런타임 에러)
-    //테스트 18 〉	실패 (런타임 에러)
-    //테스트 19 〉	통과 (19.87ms, 86.4MB)
-    //테스트 20 〉	실패 (런타임 에러)
-    //테스트 21 〉	실패 (런타임 에러)
-    //테스트 22 〉	통과 (40.29ms, 95.7MB)
-    //테스트 23 〉	실패 (런타임 에러)
-    //테스트 24 〉	통과 (31.63ms, 100MB)
-    //테스트 25 〉	통과 (29.45ms, 107MB)
-    //테스트 26 〉	실패 (런타임 에러)
-    //테스트 27 〉	실패 (런타임 에러)
-    //테스트 28 〉	통과 (2.43ms, 71.7MB)
-    //정확성: 50.0
-    //합계: 50.0 / 100.0
+    //테스트 1 〉	통과 (16.95ms, 70.1MB)
+    //테스트 2 〉	실패 (18.94ms, 86.7MB)
+    //테스트 3 〉	통과 (26.86ms, 85.1MB)
+    //테스트 4 〉	통과 (28.66ms, 75.1MB)
+    //테스트 5 〉	실패 (0.03ms, 74.4MB)
+    //테스트 6 〉	실패 (32.16ms, 78.5MB)
+    //테스트 7 〉	통과 (25.13ms, 80.8MB)
+    //테스트 8 〉	통과 (23.58ms, 77.5MB)
+    //테스트 9 〉	통과 (25.17ms, 73.2MB)
+    //테스트 10 〉	통과 (29.03ms, 78.3MB)
+    //테스트 11 〉	실패 (18.93ms, 75MB)
+    //테스트 12 〉	실패 (23.97ms, 89.2MB)
+    //테스트 13 〉	통과 (20.93ms, 81.9MB)
+    //테스트 14 〉	실패 (23.58ms, 83.6MB)
+    //테스트 15 〉	실패 (23.10ms, 72.2MB)
+    //테스트 16 〉	통과 (21.66ms, 83MB)
+    //테스트 17 〉	실패 (23.80ms, 90MB)
+    //테스트 18 〉	통과 (19.68ms, 80.5MB)
+    //테스트 19 〉	통과 (25.62ms, 82.5MB)
+    //테스트 20 〉	통과 (23.77ms, 82.1MB)
+    //테스트 21 〉	통과 (28.67ms, 91.2MB)
+    //테스트 22 〉	통과 (32.32ms, 80.6MB)
+    //테스트 23 〉	통과 (33.33ms, 77.1MB)
+    //테스트 24 〉	통과 (26.31ms, 92.4MB)
+    //테스트 25 〉	통과 (36.53ms, 85.1MB)
+    //테스트 26 〉	실패 (50.15ms, 75.8MB)
+    //테스트 27 〉	실패 (72.45ms, 93.1MB)
+    //테스트 28 〉	실패 (14.14ms, 77.3MB)
+    //채점 결과
+    //정확성: 62.0
+    //합계: 62.0 / 100.0
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
         int splitSize = 1;
 
         while (splitSize <= s.length() / 2) {
             String compressedStr = new String("");
-            StringBuilder sb;
+            String holder = "";
+            int count = 1;
             for (int i = 0; i < s.length(); i += splitSize) {
                 String subStr;
                 if (i + splitSize > s.length()) {
@@ -50,27 +52,35 @@ public class Solution {
                     subStr = s.substring(i, i + splitSize);
                 }
 
-                if (compressedStr.isEmpty()) {
-                    compressedStr = "1" + subStr;
+                if (holder.isEmpty()) {
+                    holder = subStr;
                     continue;
                 }
 
-                if (compressedStr.substring(compressedStr.length()-splitSize)
-                        .equals(subStr)) {
-                    sb = new StringBuilder(compressedStr);
-                    int wordCount = Integer.parseInt(
-                            compressedStr.substring(compressedStr.length()-1-splitSize, compressedStr.length()-splitSize));
-                    sb.setCharAt(compressedStr.length()-1-splitSize, Character.forDigit(wordCount+1, 10));
-                    compressedStr = sb.toString();
+                if (i + splitSize >= s.length()) {
+                    if (holder.equals(subStr)) {
+                        count++;
+                        compressedStr = compressedStr + count + holder;
+                        break;
+                    }
+
+                    compressedStr = compressedStr + holder + subStr;
+                    break;
+                }
+
+                if (subStr.equals(holder)) {
+                    count++;
                     continue;
                 }
 
-                sb = new StringBuilder(compressedStr);
-                sb.append("1" + subStr);
-                compressedStr = sb.toString();
+                if (count < 2) {
+                    compressedStr = compressedStr + holder;
+                } else {
+                    compressedStr = compressedStr + count + holder;
+                }
+                holder = subStr;
+                count = 1;
             }
-
-            compressedStr = compressedStr.replaceAll("1", "");
 
             if (compressedStr.length() < answer) {
                 answer = compressedStr.length();
@@ -78,7 +88,6 @@ public class Solution {
 
             splitSize++;
         }
-
         return answer;
     }
 }
