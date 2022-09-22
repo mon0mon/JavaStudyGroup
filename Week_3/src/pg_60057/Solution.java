b72f557aa1110e0db43e170ad9644f2db1b4f8e1
mon0mon <mon0mon@naver.com>
2022-09-22 09:47:37 +0900

    pg_600057 fail3

--- Week_3/src/pg_60057/Solution.java
+++ Week_3/src/pg_60057/Solution.java
@@ -1,42 +1,43 @@
 package pg_60057;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/60057
 public class Solution {
     public static void main(String[] args) {
-//        System.out.println(solution("ababcdcdababcdcd"));
+        System.out.println(solution("aabbaccc"));
     }
 
-    //테스트 1 〉	통과 (16.95ms, 70.1MB)
-    //테스트 2 〉	실패 (18.94ms, 86.7MB)
-    //테스트 3 〉	통과 (26.86ms, 85.1MB)
-    //테스트 4 〉	통과 (28.66ms, 75.1MB)
-    //테스트 5 〉	실패 (0.03ms, 74.4MB)
-    //테스트 6 〉	실패 (32.16ms, 78.5MB)
-    //테스트 7 〉	통과 (25.13ms, 80.8MB)
-    //테스트 8 〉	통과 (23.58ms, 77.5MB)
-    //테스트 9 〉	통과 (25.17ms, 73.2MB)
-    //테스트 10 〉	통과 (29.03ms, 78.3MB)
-    //테스트 11 〉	실패 (18.93ms, 75MB)
-    //테스트 12 〉	실패 (23.97ms, 89.2MB)
-    //테스트 13 〉	통과 (20.93ms, 81.9MB)
-    //테스트 14 〉	실패 (23.58ms, 83.6MB)
-    //테스트 15 〉	실패 (23.10ms, 72.2MB)
-    //테스트 16 〉	통과 (21.66ms, 83MB)
-    //테스트 17 〉	실패 (23.80ms, 90MB)
-    //테스트 18 〉	통과 (19.68ms, 80.5MB)
-    //테스트 19 〉	통과 (25.62ms, 82.5MB)
-    //테스트 20 〉	통과 (23.77ms, 82.1MB)
-    //테스트 21 〉	통과 (28.67ms, 91.2MB)
-    //테스트 22 〉	통과 (32.32ms, 80.6MB)
-    //테스트 23 〉	통과 (33.33ms, 77.1MB)
-    //테스트 24 〉	통과 (26.31ms, 92.4MB)
-    //테스트 25 〉	통과 (36.53ms, 85.1MB)
-    //테스트 26 〉	실패 (50.15ms, 75.8MB)
-    //테스트 27 〉	실패 (72.45ms, 93.1MB)
-    //테스트 28 〉	실패 (14.14ms, 77.3MB)
+    //테스트 1 〉	통과 (18.90ms, 78.9MB)
+    //테스트 2 〉	실패 (19.33ms, 84.3MB)
+    //테스트 3 〉	실패 (19.18ms, 85.7MB)
+    //테스트 4 〉	통과 (20.13ms, 79.6MB)
+    //테스트 5 〉	실패 (0.03ms, 77.6MB)
+    //테스트 6 〉	실패 (18.49ms, 72.8MB)
+    //테스트 7 〉	통과 (19.79ms, 90.3MB)
+    //테스트 8 〉	통과 (20.60ms, 85.8MB)
+    //테스트 9 〉	통과 (20.91ms, 75.7MB)
+    //테스트 10 〉	통과 (28.40ms, 80.7MB)
+    //테스트 11 〉	실패 (22.52ms, 77.5MB)
+    //테스트 12 〉	실패 (20.54ms, 75.6MB)
+    //테스트 13 〉	통과 (19.00ms, 75.3MB)
+    //테스트 14 〉	실패 (16.94ms, 75MB)
+    //테스트 15 〉	실패 (17.77ms, 80.6MB)
+    //테스트 16 〉	통과 (11.54ms, 74.6MB)
+    //테스트 17 〉	실패 (17.44ms, 84MB)
+    //테스트 18 〉	통과 (17.20ms, 81.5MB)
+    //테스트 19 〉	통과 (18.87ms, 81.4MB)
+    //테스트 20 〉	통과 (20.51ms, 82.6MB)
+    //테스트 21 〉	통과 (28.63ms, 93.4MB)
+    //테스트 22 〉	통과 (29.44ms, 80.7MB)
+    //테스트 23 〉	통과 (27.38ms, 86.2MB)
+    //테스트 24 〉	통과 (36.46ms, 87MB)
+    //테스트 25 〉	통과 (32.20ms, 84.4MB)
+    //테스트 26 〉	실패 (28.02ms, 89.9MB)
+    //테스트 27 〉	실패 (27.75ms, 81.7MB)
+    //테스트 28 〉	실패 (22.06ms, 85MB)
     //채점 결과
-    //정확성: 62.0
-    //합계: 62.0 / 100.0
-    public int solution(String s) {
+    //정확성: 58.0
+    //합계: 58.0 / 100.0
+    public static int solution(String s) {
         int answer = Integer.MAX_VALUE;
         int splitSize = 1;
 
@@ -57,23 +58,19 @@ public class Solution {
                     continue;
                 }
 
-                if (i + splitSize >= s.length()) {
-                    if (holder.equals(subStr)) {
-                        count++;
-                        compressedStr = compressedStr + count + holder;
-                        break;
-                    }
-
-                    compressedStr = compressedStr + holder + subStr;
-                    break;
-                }
-
                 if (subStr.equals(holder)) {
                     count++;
+                    if (i + splitSize >= s.length()) {
+                        compressedStr = compressedStr + count + holder;
+                    }
                     continue;
                 }
 
                 if (count < 2) {
+                    if (i + splitSize >= s.length()) {
+                        compressedStr = compressedStr + holder + subStr;
+                        break;
+                    }
                     compressedStr = compressedStr + holder;
                 } else {
                     compressedStr = compressedStr + count + holder;
---
+++ Week_3/src/pg_60057/courses30_lessons60057.java
@@ -0,0 +1,80 @@
+package pg_60057;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java
+ */
+public class courses30_lessons60057 {
+
+    /**
+     * @return parseUnit 기준으로 압축된 문자열 길이
+     */
+    private static int compressionWord(String fullString, int parseUnit) {
+        String standardStr;
+        String compareStr;
+        int compressionCount = 1;
+        StringBuilder sb = new StringBuilder();
+
+        standardStr = fullString.substring(0, parseUnit);
+        for (int i = parseUnit; i < fullString.length();) {
+            int parseStartIndex = i;
+            int parseEndIndex = i += parseUnit;
+            if(parseEndIndex > fullString.length()) {
+                parseEndIndex = fullString.length();
+            }
+
+            compareStr = fullString.substring(parseStartIndex, parseEndIndex);
+
+            if (standardStr.equals(compareStr)) {
+                compressionCount++;
+            } else {
+                if (compressionCount != 1) {
+                    sb.append(compressionCount);
+                }
+                sb.append(standardStr);
+                compressionCount = 1;
+                standardStr = compareStr;
+            }
+        }
+        if (compressionCount != 1) {
+            sb.append(compressionCount);
+        }
+        sb.append(standardStr);
+
+        // System.out.println(sb);
+        return sb.length();
+    }
+
+
+
+    public static int solution(String s) {
+        int shortestLength = s.length();
+        for (int i = 1; i <= s.length() / 2; i++) {
+            int length = compressionWord(s, i);
+
+            // System.out.println(length);
+
+            if (length < shortestLength) {
+                shortestLength = length;
+            }
+        }
+        return shortestLength;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "aabbaccc";
+        System.out.println(solution(s1));
+
+        String s2 = "ababcdcdababcdcd";
+        System.out.println(solution(s2));
+
+        String s3 = "abcabcdede";
+        System.out.println(solution(s3));
+
+        String s4 = "abcabcabcabcdededededede";
+        System.out.println(solution(s4));
+
+        String s5 = "xababcdcdababcdcd";
+        System.out.println(solution(s5));
+    }
+
+}
