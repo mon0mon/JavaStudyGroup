346a0cf6117c4b09dab620e6fc21a6b9099343d0
mon0mon <mon0mon@naver.com>
2022-09-22 10:06:53 +0900

    pg_600057 fail4

--- Week_3/src/pg_60057/Solution.java
+++ Week_3/src/pg_60057/Solution.java
@@ -6,34 +6,34 @@ public class Solution {
         System.out.println(solution("aabbaccc"));
     }
 
-    //테스트 1 〉	통과 (18.90ms, 78.9MB)
-    //테스트 2 〉	실패 (19.33ms, 84.3MB)
-    //테스트 3 〉	실패 (19.18ms, 85.7MB)
-    //테스트 4 〉	통과 (20.13ms, 79.6MB)
-    //테스트 5 〉	실패 (0.03ms, 77.6MB)
-    //테스트 6 〉	실패 (18.49ms, 72.8MB)
-    //테스트 7 〉	통과 (19.79ms, 90.3MB)
-    //테스트 8 〉	통과 (20.60ms, 85.8MB)
-    //테스트 9 〉	통과 (20.91ms, 75.7MB)
-    //테스트 10 〉	통과 (28.40ms, 80.7MB)
-    //테스트 11 〉	실패 (22.52ms, 77.5MB)
-    //테스트 12 〉	실패 (20.54ms, 75.6MB)
-    //테스트 13 〉	통과 (19.00ms, 75.3MB)
-    //테스트 14 〉	실패 (16.94ms, 75MB)
-    //테스트 15 〉	실패 (17.77ms, 80.6MB)
-    //테스트 16 〉	통과 (11.54ms, 74.6MB)
-    //테스트 17 〉	실패 (17.44ms, 84MB)
-    //테스트 18 〉	통과 (17.20ms, 81.5MB)
-    //테스트 19 〉	통과 (18.87ms, 81.4MB)
-    //테스트 20 〉	통과 (20.51ms, 82.6MB)
-    //테스트 21 〉	통과 (28.63ms, 93.4MB)
-    //테스트 22 〉	통과 (29.44ms, 80.7MB)
-    //테스트 23 〉	통과 (27.38ms, 86.2MB)
-    //테스트 24 〉	통과 (36.46ms, 87MB)
-    //테스트 25 〉	통과 (32.20ms, 84.4MB)
-    //테스트 26 〉	실패 (28.02ms, 89.9MB)
-    //테스트 27 〉	실패 (27.75ms, 81.7MB)
-    //테스트 28 〉	실패 (22.06ms, 85MB)
+    //테스트 1 〉	통과 (10.99ms, 81.4MB)
+    //테스트 2 〉	실패 (11.88ms, 87.3MB)
+    //테스트 3 〉	실패 (12.96ms, 70.8MB)
+    //테스트 4 〉	통과 (9.22ms, 76.9MB)
+    //테스트 5 〉	실패 (0.01ms, 76.2MB)
+    //테스트 6 〉	실패 (10.71ms, 79.1MB)
+    //테스트 7 〉	통과 (14.10ms, 84.8MB)
+    //테스트 8 〉	통과 (13.61ms, 74.3MB)
+    //테스트 9 〉	통과 (13.04ms, 75.7MB)
+    //테스트 10 〉	통과 (16.68ms, 78.4MB)
+    //테스트 11 〉	실패 (10.64ms, 82.9MB)
+    //테스트 12 〉	실패 (9.32ms, 73.5MB)
+    //테스트 13 〉	통과 (13.11ms, 80.4MB)
+    //테스트 14 〉	실패 (9.93ms, 79.8MB)
+    //테스트 15 〉	실패 (11.97ms, 77.4MB)
+    //테스트 16 〉	통과 (1.23ms, 79.8MB)
+    //테스트 17 〉	실패 (15.91ms, 77.1MB)
+    //테스트 18 〉	통과 (15.23ms, 80.5MB)
+    //테스트 19 〉	통과 (12.78ms, 77.8MB)
+    //테스트 20 〉	통과 (22.88ms, 81.5MB)
+    //테스트 21 〉	통과 (12.31ms, 71.7MB)
+    //테스트 22 〉	통과 (12.65ms, 84.5MB)
+    //테스트 23 〉	통과 (17.34ms, 84.1MB)
+    //테스트 24 〉	통과 (13.96ms, 73.3MB)
+    //테스트 25 〉	통과 (18.95ms, 79.3MB)
+    //테스트 26 〉	실패 (18.78ms, 83.2MB)
+    //테스트 27 〉	실패 (14.75ms, 85.8MB)
+    //테스트 28 〉	실패 (10.60ms, 76.5MB)
     //채점 결과
     //정확성: 58.0
     //합계: 58.0 / 100.0
@@ -43,6 +43,7 @@ public class Solution {
 
         while (splitSize <= s.length() / 2) {
             String compressedStr = new String("");
+            StringBuilder sb = new StringBuilder("");
             String holder = "";
             int count = 1;
             for (int i = 0; i < s.length(); i += splitSize) {
@@ -61,26 +62,26 @@ public class Solution {
                 if (subStr.equals(holder)) {
                     count++;
                     if (i + splitSize >= s.length()) {
-                        compressedStr = compressedStr + count + holder;
+                        sb.append(count + holder);
                     }
                     continue;
                 }
 
                 if (count < 2) {
                     if (i + splitSize >= s.length()) {
-                        compressedStr = compressedStr + holder + subStr;
+                        sb.append(holder + subStr);
                         break;
                     }
-                    compressedStr = compressedStr + holder;
+                    sb.append(holder);
                 } else {
-                    compressedStr = compressedStr + count + holder;
+                    sb.append(count + holder);
                 }
                 holder = subStr;
                 count = 1;
             }
 
-            if (compressedStr.length() < answer) {
-                answer = compressedStr.length();
+            if (sb.length() < answer) {
+                answer = sb.length();
             }
 
             splitSize++;
