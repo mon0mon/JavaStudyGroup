4d52e51e3daaec8ebc42488cb2bae3380ecb59b5
mon0mon <mon0mon@naver.com>
2023-01-10 20:55:20 +0900

    update

--- Week_13/src/Main.java
+++ Week_13/src/Main.java
@@ -10,23 +10,29 @@ public class Main {
                 .forEach(System.out::println);
     }
 
-    //  십진수 -> 이진수 변환
-    //  변환한 이진수를 바탕으로 지도 생성 (arr1, arr2)
+    //  1) 십진수 -> 이진수 변환
+    //  2) 변환한 이진수를 바탕으로 지도 생성 (arr1, arr2)
         //  1 -> #(벽)
         //  0 -> ' '(길)
-    //  지도1과 지도2를 합쳐서 지도3을 생성
-    //  생성된 지도3을 String[] 형식으로 출력
+    //  3) 지도1과 지도2를 합쳐서 지도3을 생
+    //  4) 생성된 지도3을 String[] 형식으로 출력
     public static String[] solution(int n, int[] arr1, int[] arr2) {
+        // 1) 십진수 -> 이진수 변환
         List<String> list1 = Arrays.stream(arr1)
                 .mapToObj(i -> convertToBinary(n, i)).collect(Collectors.toList());
         List<String> list2 = Arrays.stream(arr2)
                 .mapToObj(i -> convertToBinary(n, i)).collect(Collectors.toList());
 
-        List<String> res = IntStream.range(0, n).mapToObj(idx -> {
+        return IntStream.range(0, n).mapToObj(idx -> {
             StringBuilder sb = new StringBuilder();
+            //  2) 변환한 이진수를 바탕으로 지도 생성 (arr1, arr2)
+
+            // 1011 0011 0111 1111
+            // list1[0][0]
             char[] list1Parse = list1.get(idx).toCharArray();
             char[] list2Parse = list2.get(idx).toCharArray();
 
+            //  3) 지도1과 지도2를 합쳐서 지도3을 생성
             for (int i = 0; i < n; i++) {
                 if (list1Parse[i] == '1' || list2Parse[i] == '1') {
                     sb.append("#");
@@ -36,10 +42,9 @@ public class Main {
                 sb.append(" ");
             }
 
+            //  4) 생성된 지도3을 String[] 형식으로 출력
             return sb.toString();
-        }).collect(Collectors.toList());
-
-        return res.toArray(new String[0]);
+        }).toArray(String[]::new);
     }
 
     public static String convertToBinary(int length, int n) {
