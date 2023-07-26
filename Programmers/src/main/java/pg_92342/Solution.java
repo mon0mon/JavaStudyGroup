c258f39b1d51c70256ffb067bcd7da46720b2748
mon0mon <mon0mon@naver.com>
2023-07-26 22:12:00 +0900

    PG_92342 양궁대회 DFS로 풀어볼 수 있다는데, 막상 코드로는 어떻게 옮겨야할 지 모르겟음... 다시 풀어보기

---
+++ Programmers/src/main/java/pg_92342/Solution.java
@@ -0,0 +1,17 @@
+package pg_92342;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().solution(5, new int[] {2,1,1,1,0,0,0,0,0,0,0});
+    }
+
+    public int[] solution(int n, int[] info) {
+        int maxScore = Integer.MIN_VALUE;
+
+
+        return null;
+    }
+}
